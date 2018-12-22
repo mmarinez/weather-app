fc33aa0946745c71f0e96e865f962d0cc10a8f0a
Manuel Marinez <marinezdev@gmail.com>
2018-12-22 11:54:40 -0400

    Set app structure

---
+++ program.py
@@ -0,0 +1,23 @@
+def greetting(self):
+    print('--------------------------')
+    print('        WEATHER APP')
+    print('--------------------------')
+
+
+def set_user_zipcode(self):
+    zip_code = input('What zipcode do you want the weather for (97201)? ')
+
+
+def get_html(self):
+    pass
+
+
+def parse_html(self, parameter_list):
+    pass
+
+
+def display(self):
+    pass
+
+if __name__ == "__main__":
+    pass
