34327fdc3015ac3485ab63646454c9826b68ccdf
Manuel Marinez <marinezdev@gmail.com>
2018-12-26 10:26:23 -0400

    Add url print functionality

--- program.py
+++ program.py
@@ -4,20 +4,23 @@ def greetting(self):
     print('--------------------------')
 
 
-def set_user_zipcode(self):
+def set_user_zipcode():
     zip_code = input('What zipcode do you want the weather for (97201)? ')
+    return zip_code
 
 
-def get_html(self):
-    pass
+def get_html(zip_code):
+    url = 'https://www.wunderground.com/weather/us/ma/brimfield/{}'.format(zip_code)
+    print(url)
 
 
 def parse_html(self, parameter_list):
     pass
 
 
-def display(self):
-    pass
+def display():
+    get_html(set_user_zipcode())
+
 
 if __name__ == "__main__":
-    pass
+    display()
